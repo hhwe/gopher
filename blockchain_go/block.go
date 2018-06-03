c04eaeb4cf8fd5e2bb9d0315219392b49949384b
hhwe <hhw12125@gmail.com>
2018-06-03 11:20:40 +0800

    proof of work

--- blockchain_go/block.go
+++ blockchain_go/block.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"strconv"
-	"bytes"
-	"crypto/sha256"
 	"time"
 )
 
@@ -12,19 +9,26 @@ type Block struct {
 	Data []byte
 	PrevBlockHash []byte
 	Hash []byte
+	Nonce int
 }
 
-func (b *Block)SetHash() {
-	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
-	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
-	hash := sha256.Sum256(headers)
-
-	b.Hash = hash[:]
-}
+//func (b *Block)SetHash() {
+//	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+//	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
+//	hash := sha256.Sum256(headers)
+//
+//	b.Hash = hash[:]
+//}
 
 func NewBlock(data string, prevBlockHash []byte) *Block {
-	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
-	block.SetHash()
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, 0}
+	//block.SetHash()
+	pow := NewProofOfWork(block)
+	nonce, hash := pow.Run()
+
+	block.Hash = hash[:]
+	block.Nonce = nonce
+
 	return block
 }
 
