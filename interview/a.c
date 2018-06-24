18ec9d398bf47ddeb610785873ce1dd72c2a91be
hhwe <hhw12125@gmail.com>
2018-06-24 22:52:47 +0800

    test slice

---
+++ interview/a.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	int n[10] = {1, 2, 3};
+
+	printf("%d\n", n[1]);
+
+	printf("%p\n", n);
+
+	return 0;
+}
--- interview/tmp.go
+++ interview/tmp.go
@@ -1,7 +1,42 @@
 package main
 
-var p map[string]int
+import (
+	"time"
+	"fmt"
+)
+
+//var p map[string]int
+
+type clockFun func() time.Time
+var cc clockFun = time.Now
+var dd = clockFun(time.Now)
+
+type notint int
+var i notint = 9
+var j = notint(2)
+
+
 
 func main() {
-	p["1"] = 1
+
+	fmt.Println(cc())
+	fmt.Println(dd())
+
+	fmt.Println(i)
+	fmt.Println(j)
+
+	var k = new(notint)
+	k = &j
+	fmt.Println("k", *k)
+
+	var arr [1e6]int
+	fmt.Printf("arr %p\n", &arr)
+
+
+	//var m map[string]int
+	//m = nil
+	//print(m, "\n")
+	//n := make(map[string]int)
+	//print(n, "\n")
+
 }
