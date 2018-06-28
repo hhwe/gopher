70eac7619a9da6eef19a53d20d407e42ab5e8e04
hhwe <hhw12125@gmail.com>
2018-06-28 23:39:36 +0800

    todo list web application

--- gin_todos/main.go
+++ gin_todos/main.go
@@ -1,63 +1,35 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"net/http"
+	"fmt"
+	"log"
 )
 
-var DB = make(map[string]string)
-
-func setupRouter() *gin.Engine {
-	// Disable Console Color
-	// gin.DisableConsoleColor()
-	r := gin.Default()
-
-	// Ping test
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(200, "pong")
-	})
-
-	// Get user value
-	r.GET("/user/:name", func(c *gin.Context) {
-		user := c.Params.ByName("name")
-		value, ok := DB[user]
-		if ok {
-			c.JSON(200, gin.H{"user": user, "value": value})
-		} else {
-			c.JSON(200, gin.H{"user": user, "status": "no value"})
-		}
-	})
-
-	// Authorized group (uses gin.BasicAuth() middleware)
-	// Same than:
-	// authorized := r.Group("/")
-	// authorized.Use(gin.BasicAuth(gin.Credentials{
-	//	  "foo":  "bar",
-	//	  "manu": "123",
-	//}))
-	authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
-		"foo":  "bar", // user:foo password:bar
-		"manu": "123", // user:manu password:123
-	}))
-
-	authorized.POST("admin", func(c *gin.Context) {
-		user := c.MustGet(gin.AuthUserKey).(string)
-
-		// Parse JSON
-		var json struct {
-			Value string `json:"value" binding:"required"`
-		}
-
-		if c.Bind(&json) == nil {
-			DB[user] = json.Value
-			c.JSON(200, gin.H{"status": "ok"})
-		}
-	})
-
-	return r
+func init() {
+	http.HandleFunc("/api/signin", signInHandle)
 }
 
 func main() {
-	r := setupRouter()
-	// Listen and Server in 0.0.0.0:8080
-	r.Run(":8080")
+
+
+	http.ListenAndServe(":8080", nil)
+}
+
+type SignInResponse struct {
+	UserID string `json:"user_id"`
+	SessionToken string `json:"session_token"`
+}
+
+func signInHandle(w http.ResponseWriter, r *http.Request) {
+	//ctx := appengine.New
+	token := r.Header.Get("Authorization")
+	userID, err := verifyToken(ctx, token)
+	if err != nil {
+		log.Printf(ctx, "%v", err)
+
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, "Sign in ")
 }
