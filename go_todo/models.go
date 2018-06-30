3005328ea07b744fadf7904cc86330b8b9ef78f3
hhwe <hhw12125@gmail.com>
2018-06-30 22:39:43 +0800

    build a mini frame

--- go_todo/models.go
+++ go_todo/models.go
@@ -1,6 +1,42 @@
 package main
 
-func init() {
+import (
+	"database/sql"
+	"fmt"
+	"time"
 
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type todo struct {
+	title    string
+	finished bool
+	created  time.Time
 }
 
+var db, err = sql.Open("mysql", "root:root@/todo?charset=utf8")
+
+func get_all(query *sql.Rows) []map[string]string {
+	column, _ := query.Columns()
+	values := make([][]byte, len(column))
+	scans := make([]interface{}, len(column))
+	for i := range values {
+		scans[i] = &values[i]
+	}
+	s := make(type, 0)
+	results := make([]map[string]string, len(column))
+	for query.Next() {
+		if err := query.Scan(scans...); err != nil {
+			fmt.Println(err)
+			return nil
+		}
+		row := make(map[string]string)
+		for k, v := range values {
+			key := column[k]
+			row[key] = string(v)
+		}
+		fmt.Println(row)
+		results = append(results, row)
+	}
+	return results
+}
--- go_todo/views.go
+++ go_todo/views.go
@@ -3,27 +3,64 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
-	"regexp"
+	"time"
 )
 
 func init() {
 	http.HandleFunc("/", indexHandle)
+
+	http.HandleFunc("/get", getHandle)
+	http.HandleFunc("/post", postHandle)
 }
 
-var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
-var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+// var templates = template.Must(template.ParseFiles("index.html"))
+// var validPath = regexp.MustCompile("^/(index)/([a-zA-Z0-9]+)$")
 
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	err := template.ExecuteTemplate(w, tmpl+".html")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
+// func renderTemplate(w http.ResponseWriter, tmpl string) {
+// 	err := templates.ExecuteTemplate(w, tmpl+".html", "halo")
+// 	if err != nil {
+// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+// 	}
+// }
 
 func indexHandle(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("hello")
-	fmt.Fprintf(w, "hello")
+
+	// renderTemplate(w, "index")
+
+	t, _ := template.ParseFiles("index.html")
+	t.Execute(w, nil)
+}
+
+func getHandle(w http.ResponseWriter, r *http.Request) {
+
+	// renderTemplate(w, "index")
+	rows, err := db.Query("select * from todo;")
+	if err != nil {
+		log.Panic(err)
+	}
+	list := get_all(rows)
+	fmt.Println(list)
+	t, _ := template.ParseFiles("templates/get.html")
+	t.Execute(w, list)
+}
+
+func postHandle(w http.ResponseWriter, r *http.Request) {
+	stmt, err := db.Prepare("INSERT todo SET title=?,finished=?,created=?")
+	if err != nil {
+		log.Panic(err)
+	}
+	res, err := stmt.Exec("reading", false, time.Now())
+	if err != nil {
+		log.Panic(err)
+	}
+	id, err := res.LastInsertId()
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Println(id)
+	http.Redirect(w, r, "/get", http.StatusFound)
 }
 
 type SignInResponse struct {
