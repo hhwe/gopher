3e94ad8fe277c2a57770cd83f542afa452ea2020
hongweihan <job.han23@gmail.com>
2018-07-29 14:22:47 +0800

    array slice map

---
+++ the-way-to-go/arrayslicemap.go
@@ -0,0 +1,163 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+func main() {
+	a := [...]string{"a", "b", "c", "d"}
+	for i := range a {
+		fmt.Println("Array item", i, "is", a[i])
+	}
+
+	arrayValue()
+
+	for i := 0; i < LIM; i++ {
+		fmt.Println(fibonacci(i))
+	}
+
+	bufferByte()
+
+	mk := make([]int, 3)
+	pk := append(mk, 1, 2, 3)
+	fmt.Println(pk)
+
+	// sl := make([]byte, 3)
+	var sl = []byte{1, 2, 3}
+	dt := make([]byte, 3)
+	sd := sAppend(sl, dt)
+	fmt.Println(sd)
+
+	var buf bytes.Buffer
+	fmt.Println(buf.Bytes())
+
+	itemIter()
+}
+
+func arrayValue() {
+	var arr [15]int
+	for i := 0; i < 15; i++ {
+		arr[i] = i
+	}
+	fv := func(arr [15]int) {
+		arr[0] = 100
+	}
+	fv(arr)
+	fmt.Println(arr)
+	fc := func(arr *[15]int) {
+		arr[0] = 100
+	}
+	fc(&arr)
+	fmt.Println(arr)
+}
+
+const LIM = 41
+
+var fibs [LIM]uint64
+
+func fibonacci(n int) (res uint64) {
+	// memoization: check if fibonacci(n) is already known in array:
+	if fibs[n] != 0 {
+		res = fibs[n]
+		return
+	}
+	if n <= 1 {
+		res = 1
+	} else {
+		res = fibonacci(n-1) + fibonacci(n-2)
+	}
+	fibs[n] = res
+	return
+}
+
+func bufferByte() {
+	var buffer bytes.Buffer
+	buffer.WriteString("hello world")
+	fmt.Print(buffer.String(), "\n")
+}
+
+func sAppend(slice, data []byte) []byte {
+	for _, i := range data {
+		slice = append(slice, i)
+	}
+	return slice
+}
+
+func itemIter() {
+	items := [...]int{10, 20, 30, 40, 50}
+	for _, item := range items {
+		item *= 2
+	}
+	fmt.Println(items)
+	for i := range items {
+		items[i] *= 2
+	}
+	fmt.Println(items)
+
+	sli := []int{10, 20, 30, 40, 50}
+	sh(sli...)
+	sumArray()
+	sumSlice(sli)
+
+	sumAndAverage(sli)
+	minSlice(sli)
+	maxSlice(sli)
+}
+
+func sh(a ...int) {
+	fmt.Println(a)
+}
+
+func sumArray() {
+	arr := [10]float32{1, 2, 3}
+	var sum float32
+	for _, i := range arr {
+		sum += i
+	}
+	fmt.Println(sum)
+}
+
+func sumSlice(sli []int) {
+	sum := 0
+	for _, i := range sli {
+		sum += i
+	}
+	fmt.Println(sum)
+}
+
+func sumAndAverage(sli []int) (int, float32) {
+	sum := 0
+	for _, i := range sli {
+		sum += i
+	}
+	fmt.Println(sum)
+
+	var average float32
+	average = float32(sum / len(sli))
+	fmt.Println(sum, average)
+	return sum, average
+}
+
+func minSlice(sli []int) int {
+	min := 0
+	// min = math.MaxInt3 // why??
+	for _, i := range sli {
+		if i < min {
+			min = i
+		}
+	}
+	fmt.Println(min)
+	return min
+}
+
+func maxSlice(sli []int) int {
+	max := 0
+	for _, i := range sli {
+		if i > max {
+			max = i
+		}
+	}
+	fmt.Println(max)
+	return max
+}
--- the-way-to-go/function.go
+++ the-way-to-go/function.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"math"
+	"strings"
+	"time"
 )
 
 var num = 10
@@ -27,6 +29,26 @@ func main() {
 
 	fmt.Println(deferReturn())
 	logArgs()
+
+	fmt.Printf("%d is even: is %t\n", 16, even(16)) // 16 is even: is true
+	fmt.Printf("%d is odd: is %t\n", 17, odd(17))
+	// 17 is odd: is true
+	fmt.Printf("%d is odd: is %t\n", 18, odd(18))
+	// 18 is odd: is false
+	fmt.Println(fibonacci(5))
+	reverseInt(10)
+	fmt.Println(factorial(18))
+	fmt.Println(strings.Map(mapping, "sdfj艰苦奋斗副k-0-23家房客的"))
+	fv := func() { fmt.Println("hello world") }
+	fmt.Printf("%T", fv)
+	fc := fibonacci1()
+	for i := 0; i < 10; i++ {
+		fmt.Println(fc())
+	}
+
+	timeIt()
+
+	logWhere()
 }
 
 func PrintValues() {
@@ -84,4 +106,118 @@ func deferReturn() (i int) {
 
 func logArgs() {
 	defer log.Println("12")
+}
+
+func even(nr int) bool {
+	if nr == 0 {
+		return true
+	}
+	return odd(RevSign(nr) - 1)
+}
+
+func odd(nr int) bool {
+	if nr == 0 {
+		return false
+	}
+	return even(RevSign(nr) - 1)
+}
+
+func RevSign(nr int) int {
+	if nr < 0 {
+		return -nr
+	}
+	return nr
+}
+
+func fibonacci(n int) (pos, value int) {
+	if n < 2 {
+		return n, 1
+	}
+	_, value1 := fibonacci(n - 1)
+	_, value2 := fibonacci(n - 2)
+	pos = n
+	value = value1 + value2
+	return
+}
+
+func fibonacci1() func() int {
+	var value1, value2 = 0, 1
+	return func() int {
+		tmp := value2
+		value1, value2 = value2, value1+value2
+		return tmp
+	}
+}
+
+func reverseInt(n int) int {
+	if n < 1 {
+		return 0
+	}
+	fmt.Println(n)
+	return reverseInt(n - 1)
+}
+
+func factorial(n int) int {
+	if n == 1 {
+		return 1
+	}
+	return n * factorial(n-1)
+}
+
+func mapping(c rune) rune {
+	if c < 255 {
+		return c
+	}
+	return 32
+}
+
+func timeIt() {
+	var result uint64
+	start := time.Now()
+	for i := 0; i < LIM; i++ {
+		result = fibonacci2(i)
+		fmt.Printf("fibonacci(%d) is: %d\n", i, result)
+	}
+	end := time.Now()
+	delta := end.Sub(start)
+	fmt.Printf("longCalculation took this amount of time: %s\n", delta)
+}
+
+func fibonacci2(n int) (res uint64) {
+	if n <= 1 {
+		res = 1
+	} else {
+		res = fibonacci2(n-1) + fibonacci2(n-2)
+	}
+	return
+}
+
+const LIM = 41
+
+var fibs [LIM]uint64
+
+func fibonacci3(n int) (res uint64) {
+	// memoization: check if fibonacci(n) is already known in array:
+	if fibs[n] != 0 {
+		res = fibs[n]
+		return
+	}
+	if n <= 1 {
+		res = 1
+	} else {
+		res = fibonacci3(n-1) + fibonacci3(n-2)
+	}
+	fibs[n] = res
+	return
+}
+
+func logWhere() {
+	where := func() {
+		_, file, line, _ := runtime.Caller(1)
+		log.Printf("%s:%d", file, line)
+	}
+	where()
+
+	log.SetFlags(log.Llongfile)
+	log.Print("213")
 }
