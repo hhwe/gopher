20229d53c240ba816e3371317f5051e37beaee29
hhew <hhw12125@gmail.com>
2018-07-30 14:44:49 +0800

    map

---
+++ the-way-to-go/map.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	mp := make(map[string]int, 2)
+	fmt.Println(mp, len(mp))
+	mp["n"] = 1
+	mp["c"] = 2
+	fmt.Println(mp, len(mp))
+	mp["d"] = 3
+	fmt.Println(mp, len(mp))
+
+	// Version A:
+	items := make([]map[int]int, 5)
+	for i := range items {
+		items[i] = make(map[int]int, 1)
+		items[i][1] = 2
+	}
+	fmt.Printf("Version A: Value of items: %v\n", items)
+
+	// Version B: NOT GOOD!
+	items2 := make([]map[int]int, 5)
+	for _, item := range items2 {
+		item = make(map[int]int, 1) // item is only a copy of the slice element.
+		item[1] = 2                 // This 'item' will be lost on the next iteration.
+	}
+	fmt.Printf("Version B: Value of items: %v\n", items2)
+
+	sortMap()
+	revKeyValue()
+}
+
+var (
+	barVal = map[string]int{
+		"alpha": 34, "bravo": 56, "charlie": 23,
+		"delta": 87, "echo": 56, "foxtrot": 12,
+		"golf": 34, "hotel": 16, "indio": 87,
+		"juliet": 65, "kili": 43, "lima": 98,
+	}
+)
+
+func sortMap() {
+	fmt.Println("unsorted:")
+	for k, v := range barVal {
+		fmt.Printf("Key: %v, Value: %v / ", k, v)
+	}
+	keys := make([]string, len(barVal))
+	i := 0
+	for k, _ := range barVal {
+		keys[i] = k
+		i++
+	}
+	sort.Strings(keys)
+	fmt.Println()
+	fmt.Println("sorted:")
+	for _, k := range keys {
+		fmt.Printf("Key: %v, Value: %v / ", k, barVal[k])
+	}
+}
+
+func revKeyValue() {
+	invMap := make(map[int]string, len(barVal))
+	for k, v := range barVal {
+		invMap[v] = k
+	}
+	fmt.Println("inverted:")
+	for k, v := range invMap {
+		fmt.Printf("Key: %v, Value: %v / ", k, v)
+	}
+}
