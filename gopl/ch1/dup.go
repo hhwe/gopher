638652269c2f11a09281214dacb41b28d7d4a5c5
hhwe <hhw12125@gmail.com>
2018-11-29 21:40:08 +0800

    view controller

--- gopl/ch1/dup.go
+++ gopl/ch1/dup.go
@@ -8,15 +8,42 @@ import (
 
 func main() {
 	counts := make(map[string]int)
-	input := bufio.NewScanner(os.Stdin)
-	for input.Scan() {
-		counts[input.Text()]++
-	}
+	// input := bufio.NewScanner(os.Stdin)
+	// for input.Scan() {
+	// 	counts[input.Text()]++
+	// }
 
-	// NOTE: ignoring potential errors from input.Err()
+	// // NOTE: ignoring potential errors from input.Err()
+	// for line, n := range counts {
+	// 	if n > 1 {
+	// 		fmt.Printf("%d\t%s\n", n, line)
+	// 	}
+	// }
+
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(f, counts)
+		}
+	}
 	for line, n := range counts {
 		if n > 1 {
 			fmt.Printf("%d\t%s\n", n, line)
 		}
 	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	// NOTE: ignoring potential errors from input.Err()
 }
