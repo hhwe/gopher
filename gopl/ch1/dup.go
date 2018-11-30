24f9d5d15af4629ab88325abeccabd48abf6572a
hhwe <hhw12125@gmail.com>
2018-11-30 21:09:13 +0800

    the go programming language

--- gopl/ch1/dup.go
+++ gopl/ch1/dup.go
@@ -3,7 +3,9 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -20,19 +22,31 @@ func main() {
 	// 	}
 	// }
 
-	files := os.Args[1:]
-	if len(files) == 0 {
-		countLines(os.Stdin, counts)
-	} else {
-		for _, arg := range files {
-			f, err := os.Open(arg)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
-				continue
-			}
-			countLines(f, counts)
+	// files := os.Args[1:]
+	// if len(files) == 0 {
+	// 	countLines(os.Stdin, counts)
+	// } else {
+	// 	for _, arg := range files {
+	// 		f, err := os.Open(arg)
+	// 		if err != nil {
+	// 			fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+	// 			continue
+	// 		}
+	// 		countLines(f, counts)
+	// 	}
+	// }
+
+	for _, filename := range os.Args[1:] {
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+			continue
+		}
+		for _, line := range strings.Split(string(data), "\n") {
+			counts[line]++
 		}
 	}
+
 	for line, n := range counts {
 		if n > 1 {
 			fmt.Printf("%d\t%s\n", n, line)
