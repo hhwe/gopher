67f43e23c8bbafb0880627575de340ddc76217c8
hhwe <hhw12125@gmail.com>
2018-12-07 10:16:52 +0800

    bitcoin block

--- build-blockchain-in-go/block.go
+++ build-blockchain-in-go/block.go
@@ -1,13 +1,44 @@
 package main
 
-// In Bitcoint specification Timestamp, PrevBlockHash, and Hash are block headers, which form a separate data structure, and transactions (Data in our case) is a separate data structure. So we’re mixing them here for simplicity
+import (
+	"bytes"
+	"crypto/sha256"
+	"strconv"
+	"time"
+)
+
+// In Bitcoint specification Timestamp, PrevBlockHash, and Hash are block headers,
+// which form a separate data structure,
+// and transactions (Data in our case) is a separate data structure.
+// So we’re mixing them here for simplicity
 type Block struct {
 	// Timestamp is the current timestamp when the block created
 	Timestamp int64
-	// Actual valuable information containing in the block
-	Data []byte
 	// Stores the hash of the previous block
 	PrevBlockHash []byte
 	// The hash of current block
 	Hash []byte
+	// Actual valuable information containing in the block
+	Data []byte
+}
+
+// Creation of a block
+func NewBlock(data string, prevBlockHash []byte) *Block {
+	block := Block{
+		Timestamp:     time.Now().Unix(),
+		PrevBlockHash: prevBlockHash,
+		Data:          []byte(data),
+	}
+	block.SetHash()
+	return &block
+}
+
+// For now, we’ll just take block fields, concatenate them,
+// and calculate a SHA-256 hash on the concatenated combination.
+func (b *Block) SetHash() {
+	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp},
+		[]byte{})
+	hash := sha256.Sum256(headers)
+	b.Hash = hash[:]
 }
